7b21041217eb96f3fba0f61505333aaae515be7a
commercialgym <159087491+commercialgym@users.noreply.github.com>
2024-05-03 18:39:58 -0400

    Comparing
    after creating a list of followers/followers, i compared the elements in following to followers, if an element of following is not found in followers it is printed to the console.

--- igfollowers.cpp
+++ igfollowers.cpp
@@ -12,17 +12,23 @@
 #include <sstream>
 #include <string>
 
-const int kMaxLines = 2000;
+const int kMaxLines = 3000;
 
 int createNamesList(std::string, std::string*);
 
-int main(int argc, char* argv)
+int main(int argc, char* argv[])
 {
+
+    if (argc < 3)
+    {
+        std::cerr << "Not enough command line arguments" << std::endl; 
+    }
     //list for followers
     std::string* followerNameArr = new std::string[kMaxLines];
 
     //change hardcoded directory to argv
-    std::string followersDir = "D:\\followers\\alexia.tu-followers.csv";
+    std::string followersDir;
+    followersDir = argv[1];
 
     int linesRead;
     linesRead = createNamesList(followersDir, followerNameArr);
@@ -31,9 +37,46 @@ int main(int argc, char* argv)
     //list for following
     std::string* followingNameArr = new std::string[kMaxLines];
 
-    std::string followingDir = "D:\\followers\\alexia.tu-following.csv";
+    std::string followingDir;
+    followingDir = argv[2];
 
-    createNamesList(followingDir, followingNameArr);
+    int totalLines;
+    totalLines = createNamesList(followingDir, followingNameArr);
+
+    //now to compare
+
+    //if element from following not found in followers, print to screen or add to array
+
+    // < = ?
+    int followsBack;
+
+    std::cout << "The people who don't follow you back are: " << std:: endl; 
+
+    //totalLines is the number of lines read from who you follow
+    for (int i = 0; i < totalLines; i++)
+    {
+        followsBack = 0;
+        //linesRead is the number of lines from those who follow you
+        for (int j = 0; j < linesRead; j++)
+        {
+            //if there is a match in usernames that means that they follow back and 
+            //there is no need to keep searching
+            if (followingNameArr[i].compare(followerNameArr[j]) == 0)
+            {
+                followsBack = 1;
+                break;
+            }
+        }
+
+        if (followsBack == 0)
+        {
+            std::cout << followingNameArr[i] << std:: endl;
+        }
+
+    }
+
+    //print the index of following that does not exist in followers
+
 
     /*
     for (int i = 0; i < linesRead; i++)
